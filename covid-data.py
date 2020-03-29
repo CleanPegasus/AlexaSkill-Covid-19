11117b4cff6cf45114b9a1719c85df6f849b3015
CleanPegasus <arunk609@gmail.com>
2020-03-29 11:09:03 +0530

    File to extract covid-19 confirmed cases and deaths

--- covid-data.py
+++ covid-data.py
@@ -1,27 +1,23 @@
 import requests
 import json
 
-response = requests.get('https://coronavirus-tracker-api.herokuapp.com/v2/locations')
 
-country = "United Kingdom"
-data = response.json()
+def world_data():
+    response = requests.get('https://coronavirus-tracker-api.herokuapp.com/v2/locations')
+    data = response.json()
+    return data["latest"]["confirmed"], data["latest"]["deaths"]
 
-#print(data["locations"][0])
-world_stats = data["latest"]
+def country_data(country):
+    response = requests.get('https://coronavirus-tracker-api.herokuapp.com/v2/locations')
+    data = response.json()
+    loc_data = data["locations"]
+    confirmed_data = []
+    death_data = []
 
-loc_data = data["locations"]
-
-confirmed_data = []
-death_data = []
-
-for location in loc_data:
-    #print(location["country"] + " " + str(location["coordinates"]))
-    if(country == location["country"]):
-        confirmed_data.append(location["latest"]["confirmed"])
-        death_data.append(location["latest"]["deaths"])
-
-confirmed = sum(confirmed_data)
-deaths = sum(death_data)
-
-print(confirmed)
-print(deaths)
+    for location in loc_data:
+        #print(location["country"] + " " + str(location["coordinates"]))
+        if(country == location["country"]):
+            confirmed_data.append(location["latest"]["confirmed"])
+            death_data.append(location["latest"]["deaths"])
+            
+    return sum(confirmed_data), sum(death_data)
